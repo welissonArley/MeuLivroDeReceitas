6599c349b3aaecdca0a164234fb0f35d634410f4
Welisson Arley <welissonarleyvs@gmail.com>
2022-06-26 15:41:46 +0100

    adicionando classes no projeto application para registrar usuario

---
+++ src/Backend/MeuLivroDeReceitas.Application/Bootstrapper.cs
@@ -0,0 +1,33 @@
+﻿using MeuLivroDeReceitas.Application.Servicos.Criptografia;
+using MeuLivroDeReceitas.Application.Servicos.Token;
+using MeuLivroDeReceitas.Application.UseCases.Usuario.Registrar;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MeuLivroDeReceitas.Application;
+
+public static class Bootstrapper
+{
+    public static void AddApplication(this IServiceCollection services, IConfiguration configuration)
+    {
+        AdcionarChaveAdicionalSenha(services, configuration);
+        AdcionarTokenJWT(services, configuration);
+
+        services.AddScoped<IRegistrarUsuarioUseCase, RegistrarUsuarioUseCase>();
+    }
+
+    private static void AdcionarChaveAdicionalSenha(IServiceCollection services, IConfiguration configuration)
+    {
+        var section = configuration.GetRequiredSection("Configuracoes:ChaveAdicionalSenha");
+
+        services.AddScoped(option => new EncriptadorDeSenha(section.Value));
+    }
+
+    private static void AdcionarTokenJWT(IServiceCollection services, IConfiguration configuration)
+    {
+        var sectionTempoDeVida = configuration.GetRequiredSection("Configuracoes:TempoVidaToken");
+        var sectionKey = configuration.GetRequiredSection("Configuracoes:ChaveToken");
+        
+        services.AddScoped(option => new TokenController(int.Parse(sectionTempoDeVida.Value), sectionKey.Value));
+    }
+}
