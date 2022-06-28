e6a9acc8aab6bd8957beadbd8aa6f40f911a65de
Welisson Arley <welissonarleyvs@gmail.com>
2022-06-28 19:33:17 +0100

    adicionando testes de unidade para o vlaidator de registrar usuario

---
+++ tests/Utilitario.ParaTestes/Requisicoes/Usuario/RequisicaoRegistrarUsuarioBuilder.cs
@@ -0,0 +1,16 @@
+﻿using Bogus;
+using MeuLivroDeReceitas.Comunicacao.Requisicoes;
+
+namespace Utilitario.ParaTestes.Requisicoes.Usuario;
+
+public class RequisicaoRegistrarUsuarioBuilder
+{
+    public static RequisicaoRegistrarUsuarioJson Instancia()
+    {
+        return new Faker<RequisicaoRegistrarUsuarioJson>()
+            .RuleFor(objeto => objeto.Nome, (opcoes) => opcoes.Person.FirstName)
+            .RuleFor(objeto => objeto.Email, (opcoes) => opcoes.Person.Email)
+            .RuleFor(objeto => objeto.Senha, (opcoes) => opcoes.Internet.Password(10))
+            .RuleFor(objeto => objeto.Telefone, (opcoes) => opcoes.Phone.PhoneNumber("## 9 ####-####"));
+    }
+}
