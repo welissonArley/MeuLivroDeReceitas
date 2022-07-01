3fb181e858ad48d3e55fd744543c197ef3cf373f
Welisson Arley <welissonarleyvs@gmail.com>
2022-07-01 13:06:36 +0100

    adincionando testes de unidade para validar registro de usuario

---
+++ tests/Validators.Test/Usuario/Registrar/RegistrarUsuarioValidatorTest.cs
@@ -0,0 +1,124 @@
+﻿using FluentAssertions;
+using MeuLivroDeReceitas.Application.UseCases.Usuario.Registrar;
+using MeuLivroDeReceitas.Exceptions;
+using Utilitario.ParaTestes.Requisicoes;
+using Xunit;
+
+namespace Validators.Test.Usuario.Registrar;
+
+public class RegistrarUsuarioValidatorTest
+{
+    [Fact]
+    public void Validar_Sucesso()
+    {
+        var validator = new RegistrarUsuarioValidator();
+
+        var requisicao = RegistrarUsuarioJsonBuilder.Construir();
+
+        var resultado = validator.Validate(requisicao);
+
+        resultado.IsValid.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Validar_Erro_Nome_Vazio()
+    {
+        var validator = new RegistrarUsuarioValidator();
+
+        var requisicao = RegistrarUsuarioJsonBuilder.Construir();
+        requisicao.Nome = string.Empty;
+
+        var resultado = validator.Validate(requisicao);
+
+        resultado.IsValid.Should().BeFalse();
+        resultado.Errors.Should().ContainSingle(e => e.ErrorMessage.Equals(ResourceMensagensDeErro.NOME_USUARIO_EMBRANCO));
+    }
+
+    [Fact]
+    public void Validar_Erro_Email_Vazio()
+    {
+        var validator = new RegistrarUsuarioValidator();
+
+        var requisicao = RegistrarUsuarioJsonBuilder.Construir();
+        requisicao.Email = string.Empty;
+
+        var resultado = validator.Validate(requisicao);
+
+        resultado.IsValid.Should().BeFalse();
+        resultado.Errors.Should().ContainSingle(e => e.ErrorMessage.Equals(ResourceMensagensDeErro.EMAIL_USUARIO_EMBRANCO));
+    }
+
+    [Fact]
+    public void Validar_Erro_Telefone_Vazio()
+    {
+        var validator = new RegistrarUsuarioValidator();
+
+        var requisicao = RegistrarUsuarioJsonBuilder.Construir();
+        requisicao.Telefone = string.Empty;
+
+        var resultado = validator.Validate(requisicao);
+
+        resultado.IsValid.Should().BeFalse();
+        resultado.Errors.Should().ContainSingle(e => e.ErrorMessage.Equals(ResourceMensagensDeErro.TELEFONE_USUARIO_EMBRANCO));
+    }
+
+    [Fact]
+    public void Validar_Erro_Senha_Vazio()
+    {
+        var validator = new RegistrarUsuarioValidator();
+
+        var requisicao = RegistrarUsuarioJsonBuilder.Construir();
+        requisicao.Senha = string.Empty;
+
+        var resultado = validator.Validate(requisicao);
+
+        resultado.IsValid.Should().BeFalse();
+        resultado.Errors.Should().ContainSingle(e => e.ErrorMessage.Equals(ResourceMensagensDeErro.SENHA_USUARIO_EMBRANCO));
+    }
+
+    [Fact]
+    public void Validar_Erro_Email_Invalido()
+    {
+        var validator = new RegistrarUsuarioValidator();
+
+        var requisicao = RegistrarUsuarioJsonBuilder.Construir();
+        requisicao.Email = "email@";
+
+        var resultado = validator.Validate(requisicao);
+
+        resultado.IsValid.Should().BeFalse();
+        resultado.Errors.Should().ContainSingle(e => e.ErrorMessage.Equals(ResourceMensagensDeErro.EMAIL_USUARIO_INVALIDO));
+    }
+
+    [Theory]
+    [InlineData(1)]
+    [InlineData(2)]
+    [InlineData(3)]
+    [InlineData(4)]
+    [InlineData(5)]
+    public void Validar_Erro_Senha_Invalida(int tamanhoSenha)
+    {
+        var validator = new RegistrarUsuarioValidator();
+
+        var requisicao = RegistrarUsuarioJsonBuilder.Construir(tamanhoSenha);
+
+        var resultado = validator.Validate(requisicao);
+
+        resultado.IsValid.Should().BeFalse();
+        resultado.Errors.Should().ContainSingle(e => e.ErrorMessage.Equals(ResourceMensagensDeErro.SENHA_USUARIO_MINIMO_SEIS_CARACTERES));
+    }
+
+    [Fact]
+    public void Validar_Erro_Telefone_Invalido()
+    {
+        var validator = new RegistrarUsuarioValidator();
+
+        var requisicao = RegistrarUsuarioJsonBuilder.Construir();
+        requisicao.Telefone = "37 89";
+
+        var resultado = validator.Validate(requisicao);
+
+        resultado.IsValid.Should().BeFalse();
+        resultado.Errors.Should().ContainSingle(e => e.ErrorMessage.Equals(ResourceMensagensDeErro.TELEFONE_USUARIO_INVALIDO));
+    }
+}
