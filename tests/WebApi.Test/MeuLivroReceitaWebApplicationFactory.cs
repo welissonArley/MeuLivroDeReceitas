54492181db5d1ad85c14c3191f01bf63ff22b2ac
Welisson Arley <welissonarleyvs@gmail.com>
2022-07-01 20:48:11 +0100

    adicionando testes de unidade para endpoint registrar usuario

---
+++ tests/WebApi.Test/MeuLivroReceitaWebApplicationFactory.cs
@@ -0,0 +1,39 @@
+﻿using MeuLivroDeReceitas.Infrastructure.AcessoRepositorio;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WebApi.Test;
+
+public class MeuLivroReceitaWebApplicationFactory<TStartup> : WebApplicationFactory<TStartup> where TStartup : class
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.UseEnvironment("Test")
+            .ConfigureServices(services =>
+            {
+                var descritor = services.SingleOrDefault(d => d.ServiceType == typeof(MeuLivroDeReceitasContext));
+                if (descritor != null)
+                    services.Remove(descritor);
+
+                var provider = services.AddEntityFrameworkInMemoryDatabase().BuildServiceProvider();
+
+                services.AddDbContext<MeuLivroDeReceitasContext>(options =>
+                {
+                    options.UseInMemoryDatabase("InMemoryDbForTesting");
+                    options.UseInternalServiceProvider(provider);
+                });
+
+                var serviceProvider = services.BuildServiceProvider();
+                
+
+                using var scope = serviceProvider.CreateScope();
+                var scopeService = scope.ServiceProvider;
+                
+                var database = scopeService.GetRequiredService<MeuLivroDeReceitasContext>();
+
+                database.Database.EnsureDeleted();
+            });
+    }
+}
