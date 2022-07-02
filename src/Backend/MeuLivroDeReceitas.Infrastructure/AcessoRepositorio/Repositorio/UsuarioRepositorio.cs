53afeb1490d96eede520e7518828133c0d9529c0
Welisson Arley <welissonarleyvs@gmail.com>
2022-07-02 16:29:53 +0100

    corringindo nome da função

--- src/Backend/MeuLivroDeReceitas.Infrastructure/AcessoRepositorio/Repositorio/UsuarioRepositorio.cs
+++ src/Backend/MeuLivroDeReceitas.Infrastructure/AcessoRepositorio/Repositorio/UsuarioRepositorio.cs
@@ -23,7 +23,7 @@ public class UsuarioRepositorio : IUsuarioWriteOnlyRepositorio, IUsuarioReadOnly
         return await _contexto.Usuarios.AnyAsync(c => c.Email.Equals(email));
     }
 
-    public async Task<Usuario> Login(string email, string senha)
+    public async Task<Usuario> RecuperarPorEmailSenha(string email, string senha)
     {
         return await _contexto.Usuarios.AsNoTracking()
             .FirstOrDefaultAsync(c => c.Email.Equals(email) && c.Senha.Equals(senha));
