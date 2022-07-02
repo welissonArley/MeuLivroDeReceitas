05d6b4a03aac568d6163aa69ae96efcb13058648
Welisson Arley <welissonarleyvs@gmail.com>
2022-07-02 19:14:33 +0100

    adicionando teste de unidade para usecase de login

--- tests/Utilitario.ParaOsTestes/Repositorios/UsuarioReadOnlyRepositorioBuilder.cs
+++ tests/Utilitario.ParaOsTestes/Repositorios/UsuarioReadOnlyRepositorioBuilder.cs
@@ -30,6 +30,13 @@ public class UsuarioReadOnlyRepositorioBuilder
         return this;
     }
 
+    public UsuarioReadOnlyRepositorioBuilder RecuperarPorEmailSenha(MeuLivroDeReceitas.Domain.Entidades.Usuario usuario)
+    {
+        _repositorio.Setup(i => i.RecuperarPorEmailSenha(usuario.Email, usuario.Senha)).ReturnsAsync(usuario);
+
+        return this;
+    }
+
     public IUsuarioReadOnlyRepositorio Construir()
     {
         return _repositorio.Object;
