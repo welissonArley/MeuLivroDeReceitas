b1ec95e3ef9194037736af14cceba615c83f6758
Welisson Arley <welissonarleyvs@gmail.com>
2022-07-03 13:19:51 +0100

    adicionando enpoint alterar senha

---
+++ src/Backend/MeuLivroDeReceitas.Api/Filtros/UsuarioAutenticadoAttribute.cs
@@ -0,0 +1,69 @@
+﻿using MeuLivroDeReceitas.Application.Servicos.Token;
+using MeuLivroDeReceitas.Comunicacao.Respostas;
+using MeuLivroDeReceitas.Domain.Repositorios.Usuario;
+using MeuLivroDeReceitas.Exceptions;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.IdentityModel.Tokens;
+using System.Threading.Tasks;
+
+namespace MeuLivroDeReceitas.Api.Filtros;
+
+public class UsuarioAutenticadoAttribute : AuthorizeAttribute, IAsyncAuthorizationFilter
+{
+    private readonly TokenController _tokenController;
+    private readonly IUsuarioReadOnlyRepositorio _repositorio;
+
+    public UsuarioAutenticadoAttribute(TokenController tokenController, IUsuarioReadOnlyRepositorio repositorio)
+    {
+        _tokenController = tokenController;
+        _repositorio = repositorio;
+    }
+
+    public async Task OnAuthorizationAsync(AuthorizationFilterContext context)
+    {
+        try
+        {
+            var token = TokenNaRequisicao(context);
+            var emailUsuario = _tokenController.RecuperarEmail(token);
+
+            var usuario = await _repositorio.RecuperarPorEmail(emailUsuario);
+
+            if (usuario is null)
+            {
+                throw new System.Exception();
+            }
+        }
+        catch (SecurityTokenExpiredException)
+        {
+            TokenExpirado(context);
+        }
+        catch
+        {
+            UsuarioSemPermissao(context);
+        }
+    }
+
+    private string TokenNaRequisicao(AuthorizationFilterContext context)
+    {
+        var authorization = context.HttpContext.Request.Headers["Authorization"].ToString();
+
+        if (string.IsNullOrWhiteSpace(authorization))
+        {
+            throw new System.Exception();
+        }
+
+        return authorization["Bearer".Length..].Trim();
+    }
+
+    private void TokenExpirado(AuthorizationFilterContext context)
+    {
+        context.Result = new UnauthorizedObjectResult(new RespostaErroJson(ResourceMensagensDeErro.TOKEN_EXPIRADO));
+    }
+
+    private void UsuarioSemPermissao(AuthorizationFilterContext context)
+    {
+        context.Result = new UnauthorizedObjectResult(new RespostaErroJson(ResourceMensagensDeErro.USUARIO_SEM_PERMISSAO));
+    }
+}
