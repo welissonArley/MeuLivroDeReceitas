cd83cad67e4763337f0b631ee762da00b647acc3
Welisson Arley <welissonarleyvs@gmail.com>
2022-07-04 18:57:37 +0100

    feat: adicionando entidades e repositorio para registrar uma nova receita

---
+++ src/Backend/MeuLivroDeReceitas.Domain/Entidades/Ingrediente.cs
@@ -0,0 +1,9 @@
+﻿namespace MeuLivroDeReceitas.Domain.Entidades
+{
+    public class Ingrediente : EntidadeBase
+    {
+        public string Produto { get; set; }
+        public string Quantidade { get; set; }
+        public long ReceitaId { get; set; }
+    }
+}
---
+++ src/Backend/MeuLivroDeReceitas.Domain/Entidades/Receita.cs
@@ -0,0 +1,11 @@
+﻿using MeuLivroDeReceitas.Domain.Enum;
+
+namespace MeuLivroDeReceitas.Domain.Entidades;
+
+public class Receita : EntidadeBase
+{
+    public string Titulo { get; set; }
+    public Categoria Categoria { get; set; }
+    public string ModoPreparo { get; set; }
+    public ICollection<Ingrediente> Ingredientes { get; set; }
+}
