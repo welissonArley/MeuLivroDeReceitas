3406541720c2d7338c60d8d91f89e0ccaabfd9e7
Welisson Arley <welissonarleyvs@gmail.com>
2022-07-05 18:45:41 +0100

    feat: adicionando endpoint para adicionar receita

---
+++ src/Backend/MeuLivroDeReceitas.Api/Controllers/ReceitasController.cs
@@ -0,0 +1,24 @@
+﻿using MeuLivroDeReceitas.Api.Filtros;
+using MeuLivroDeReceitas.Application.UseCases.Receita.Registrar;
+using MeuLivroDeReceitas.Comunicacao.Requisicoes;
+using MeuLivroDeReceitas.Comunicacao.Respostas;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace MeuLivroDeReceitas.Api.Controllers;
+
+[ServiceFilter(typeof(UsuarioAutenticadoAttribute))]
+public class ReceitasController : MeuLivroDeReceitasController
+{
+    [HttpPost]
+    [ProducesResponseType(typeof(RespostaReceitaJson), StatusCodes.Status201Created)]
+    public async Task<IActionResult> Registrar(
+        [FromServices] IRegistrarReceitaUseCase useCase,
+        [FromBody] RequisicaoRegistrarReceitaJson requisicao)
+    {
+        var resposta = await useCase.Executar(requisicao);
+
+        return Created(string.Empty, resposta);
+    }
+}
