52392a43cc811bce40bb7253348df6c3643b5496
Welisson Arley <welissonarleyvs@gmail.com>
2022-07-09 12:44:34 +0100

    feat: adicionando repositorio para recuperar receitas

---
+++ src/Backend/MeuLivroDeReceitas.Domain/Repositorios/Receita/IReceitaReadOnlyRepositorio.cs
@@ -0,0 +1,5 @@
+﻿namespace MeuLivroDeReceitas.Domain.Repositorios.Receita;
+public interface IReceitaReadOnlyRepositorio
+{
+    Task<IList<Entidades.Receita>> RecuperarTodasDoUsuario(long usuarioId);
+}
--- src/Backend/MeuLivroDeReceitas.Infrastructure/AcessoRepositorio/Repositorio/ReceitaRepositorio.cs
+++ src/Backend/MeuLivroDeReceitas.Infrastructure/AcessoRepositorio/Repositorio/ReceitaRepositorio.cs
@@ -1,9 +1,10 @@
 ﻿using MeuLivroDeReceitas.Domain.Entidades;
 using MeuLivroDeReceitas.Domain.Repositorios.Receita;
+using Microsoft.EntityFrameworkCore;
 
 namespace MeuLivroDeReceitas.Infrastructure.AcessoRepositorio.Repositorio;
 
-public class ReceitaRepositorio : IReceitaWriteOnlyRepositorio
+public class ReceitaRepositorio : IReceitaWriteOnlyRepositorio, IReceitaReadOnlyRepositorio
 {
     private readonly MeuLivroDeReceitasContext _contexto;
 
@@ -11,7 +12,14 @@ public class ReceitaRepositorio : IReceitaWriteOnlyRepositorio
     {
         _contexto = contexto;
     }
-    
+
+    public async Task<IList<Receita>> RecuperarTodasDoUsuario(long usuarioId)
+    {
+        return await _contexto.Receitas.AsNoTracking()
+            .Include(r => r.Ingredientes)
+            .Where(r => r.UsuarioId == usuarioId).ToListAsync();
+    }
+
     public async Task Registrar(Receita receita)
     {
         await _contexto.Receitas.AddAsync(receita);
